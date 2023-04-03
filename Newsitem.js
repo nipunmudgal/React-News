2fc8ae983d354fe2c5d0b23bd2662da01644a0f3
nipunmudgal <80744559+nipunmudgal@users.noreply.github.com>
2023-04-04 02:01:42 +0530

    Update Newsitem.js

--- Newsitem.js
+++ Newsitem.js
@@ -2,14 +2,24 @@ import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props; //DESTRUCTURING
+    let {title, description, imageUrl, newsUrl, author, date, source  } = this.props; //DESTRUCTURING
     return (
       <div className="my-3">
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card">
+      <div style={{
+        display: 'flex',
+        justifyContent:'flex-end',
+        position:'absolute',
+        right:'0'}
+      }>
+
+        <span className="badge rounded-pill bg-danger"> {source} </span>
+        </div>
           <img src={!imageUrl?"https://www.coindesk.com/resizer/vmPh6zN1Oshmo7QsQ6qLal0LiZ4=/1200x628/center/middle/cloudfront-us-east-1.images.arcpublishing.com/coindesk/BAETRQU2J5DUNPL2GGWQMZ6CLY.jpg":imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
+            <h5 className="card-title">{title} </h5>
             <p className="card-text"> {description}</p>
+            <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>      
             <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
           </div>
         </div>
@@ -20,3 +30,4 @@ export class Newsitem extends Component {
 
 export default Newsitem;
  //target="_blank" new tab m open
+//  style={{left:'90%',zIndex:1}}
